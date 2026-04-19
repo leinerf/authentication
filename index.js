f32a5cbabcbc20cb1b27d7fe81cb875282e88955
Freniel Zabala <frenielzabala@MacBookPro.attlocal.net>
2026-04-19 15:27:00 -0700

    modify your own secrets

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt"
 import session from "express-session";
 import passport from "passport";
 import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2"
 import env from "dotenv";
 
 env.config();
@@ -36,19 +37,62 @@ app.use(passport.session());
 
 app.get("/secrets", (req, res) => {
     if (req.isAuthenticated()) {
-        return res.render("secrets.ejs");
+        console.log(req.user);
+        return res.render("secrets.ejs", {...req.user });
     } else {
         return res.redirect("/login")
     }
+})
+
+app.get("/submit", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("submit.ejs")
+    } else {
+        return res.redirect("/login")
+    }
+})
+
+app.post("/submit", async(req, res) => {
+    if (req.isAuthenticated()) {
+        try {
+            await db.query("update users set secret=$1 where uid=$2", [req.body.secret, parseInt(req.user.uid)])
+            req.user.secret = req.body.secret
+            return res.redirect("/secrets")
+        } catch (err) {
+            console.error(err)
+            return res.send("Something went wrong with submitting secret")
+        }
+    }
+    return res.redirect("/")
 })
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
 });
 
+app.get("/auth/google/", passport.authenticate("google", {
+    scope: ["profile", "email"],
+}));
+
+app.get("/auth/google/secrets",
+    passport.authenticate("google", {
+        successRedirect: "/secrets",
+        failureRedirect: "/login",
+    })
+);
+
 app.get("/login", (req, res) => {
     res.render("login.ejs");
 });
+
+app.get("/logout", (req, res) => {
+    return req.logout((err) => {
+        if (err) {
+            console.error(err);
+        }
+        return res.redirect("/")
+    });
+})
 
 app.get("/register", (req, res) => {
     res.render("register.ejs");
@@ -69,7 +113,7 @@ app.post("/register", async(req, res) => {
             const salt = bcrypt.genSaltSync(saltRounds);
             const hash = bcrypt.hashSync(password, salt);
             const result = await db.query(
-                "INSERT INTO users (email, salt, hash) VALUES ($1, $2, $3) returning *", [email, salt, hash]
+                "INSERT INTO users (email, password) VALUES ($1, $2) returning *", [email, password]
             );
             const user = result.rows[0];
             return req.login(user, (err) => {
@@ -90,14 +134,14 @@ app.post("/login", passport.authenticate("local", {
     failureRedirect: "/login"
 }));
 
-passport.use(new Strategy(async function verify(username, password, cb) {
+passport.use("local", new Strategy(async function verify(username, password, cb) {
     try {
         const result = await db.query("SELECT * FROM users WHERE email = $1", [
             username,
         ]);
         if (result.rows.length > 0) {
             const user = result.rows[0];
-            if (bcrypt.compareSync(password, user.hash)) {
+            if (bcrypt.compareSync(password, user.password)) {
                 return cb(null, user)
             } else {
                 return cb(null, false)
@@ -107,6 +151,26 @@ passport.use(new Strategy(async function verify(username, password, cb) {
         }
     } catch (err) {
         return cb(err);
+    }
+}))
+
+passport.use("google", new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+}, async(accessToken, refreshToken, profile, cb) => {
+    console.log(profile);
+    try {
+        const result = await db.query("select * from users where email=$1", [profile.email]);
+        if (result.rows.length === 0) {
+            const newUser = await db.query("INSERT INTO users (email, password) VALUES ($1, $2) returning *", [profile.email, "google"])
+            cb(null, newUser.rows[0])
+        } else {
+            cb(null, result.rows[0])
+        }
+    } catch (err) {
+        cb(err)
     }
 }))
 
